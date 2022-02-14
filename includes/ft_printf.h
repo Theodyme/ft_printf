34e1674f2c169a7a19e624aad127a3ed7c8fd6aa
Florine PLACE <flplace@e1r10p21.42.fr>
2022-02-14 18:50:11 +0100

    deleting includes file yet again

--- includes/ft_printf.h
+++
@@ -1,9 +0,0 @@
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-
-# include <unistd.h>
-# include "../libft/libft.h"
-
-int	ft_check_base(char *base, int baselen);
-void	ft_putchar(char b);
-void	ft_putnbr_base(int nbr, char *base);
